b9bba91dd690e6d63a2509901713d0ef93461818
kinen123 <gamerkinen4@gmail.com>
2026-03-13 21:00:32 -1200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    CutebotPro.distanceRunning(CutebotProOrientation.Advance, 100, CutebotProDistanceUnits.Cm)
+})
+let strip = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
+basic.forever(function () {
+    CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0xff0000)
+    strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    basic.pause(500)
+    CutebotPro.colorLight(CutebotProRGBLight.RGBA, 0x0000ff)
+    strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(500)
+})
